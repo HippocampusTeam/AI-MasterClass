a4e3191ff09684d710de61e9dd57c681b4116aef
DablSi <duck.shelter@gmail.com>
2021-07-16 17:20:53 +0300

    Comments, task and manual

--- solution.py
+++ solution.py
@@ -9,7 +9,6 @@ def calculate(input, weights, bias):
     return activation_func(output + bias)
 
 
-# inputs
 first = [8.5, 9.5, 9.9, 9.0]
 second = [0.65, 0.8, 0.8, 0.9]
 third = [1.2, 1.3, 0.5, 1.0]
